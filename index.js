b99f8afcdea7d7cd23ad21a5af6d44d1c77c90b6
unknown <sago980302@hotmail.com>
2025-05-07 14:47:47 -0500

    callback adjust to the old one

--- index.js
+++ index.js
@@ -231,8 +231,7 @@ function modificarCantidad(idProducto, cambio) {
       const monto = carrito.reduce((sum, item) => sum + item.precio, 0);
       const imagenUrl = obtenerUrlAbsoluta(carrito[0].imagen);
       const mensaje = `Hola! Realicé el pago exitoso de: ${descripcion} por $${monto.toLocaleString('es-CO')} COP.`;
-      //const callback_url = `https://wa.me/+573177657335?text=${encodeURIComponent(mensaje)}`;
-      const callback_url = "https://camerinojipsandbox.netlify.app/boldwebhook";
+      const callback_url = `https://wa.me/+573177657335?text=${encodeURIComponent(mensaje)}`;
 
       const raw = JSON.stringify({
         monto,
